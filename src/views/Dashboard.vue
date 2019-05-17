10c33f84efb832a675f02ca96c2743f6ad061546
Bruno Ferreira Lima <bruno@bruno.razer>
2019-05-17 18:03:18 -0300

    criado ordenaçao por nome de projeto e de pessoa

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,8 +3,24 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-      
-      
+
+      <v-layout row class="mb-3">
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="ordenarPor('title')" slot="activator">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">Por nome de Projeto</span>
+          </v-btn>
+          <span>Ordenar projetos por nome</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="ordenarPor('person')" slot="activator">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">Por nome da Pessoa</span>
+          </v-btn>
+          <span>Ordenar pessoas por nome</span>
+        </v-tooltip>
+      </v-layout>
+
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -43,6 +59,11 @@ export default {
         { title: 'Design thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'Completo', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Criar um fórum da comunidade', person: 'Gouken', due: '20th Oct 2018', status: 'Encerrado', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+    }
+  },
+  methods: {
+    ordenarPor(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
     }
   }
 }
