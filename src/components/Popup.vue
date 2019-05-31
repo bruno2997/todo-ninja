4f4901ed2c347a943f664986727bffcd6c164aee
Bruno Ferreira Lima <bruno@bruno.razer>
2019-05-31 18:03:30 -0300

    criado formulario para adicionar projeto e colocado date-picker para selecionar o vencimento

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,12 +5,45 @@
       <v-card-title>
         <h2>Adicione um novo projeto</h2>
       </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+
+          <v-menu>
+            <v-text-field :value="formattedDate" slot="activator" label="Data de Vencimento" prepend-icon="date_range"></v-text-field>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+
+          <v-spacer></v-spacer>
+          
+          <v-btn flat class="success mx-0 mt-3" @click="submit">Adicionar Projeto</v-btn>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-export default {
+import format from 'date-fns/format'
 
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+      due: null
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content)
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, 'DD/MM/YYYY') : ''
+    }
+  }
 }
 </script>
